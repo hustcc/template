faaec07d2b39df1935dedf93f36585e8fbb12153
hustcc <i@hust.cc>
2019-10-16 14:14:27 +0800

    feat: add rollup for dist

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import { uglify } from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
+
+module.exports = [{
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/template.min.js',
+    name: 'template',
+    format: 'umd',
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(),
+    typescript(),
+    uglify(),
+  ],
+}];
